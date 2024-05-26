c0e16bc82bf2aabc4afffc8ca45872a757b2a676
Roadflare <roadflare@tuta.io>
2024-05-26 12:53:05 +0200

    Renaming variables + Scraper

--- scraper.py
+++ scraper.py
@@ -1,23 +1,30 @@
 import requests
 
-teams = ("ATL", "BOS", "NJN", "CHA", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", 
+all_teams = ("ATL", "BOS", "NJN", "CHA", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", 
 "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NOH", "NYK", "OKC", 
 "ORL", "PHI", "PHO", "POR", "SAC", "SAS", "TOR", "UTA", "WAS")
 
 
-def Input() -> list:
+def User_input() -> tuple[tuple, tuple]:
     """Input function to pick teams and/or start/end year"""
     team, year = [], ()
-    teams_str = "\n".join([f"{teams.index(x)}: {x}" for x in teams])
-    print("Select numbers from the teams to scrape. Type None to skip. Space seperated!")    
+    teams_str = "\n".join([f"{all_teams.index(x)}: {x}" for x in all_teams])
+    print("Select numbers from the teams to scrape. Leave empty for all. Space seperated!")
     print(teams_str)
     select = input()
     year = (input("Start year: \n"), input("End year: \n"))
-    team = team.append()
+    if select:
+        for item in select.split():
+            team.append(all_teams[int(item)])
+    else:
+        team = all_teams
+    team = tuple(team)
     return team, year
 
-def Scraper(team=teams: tuple[str], year=(1980, 2024): tuple[int, int]) -> int:
+def Scraper(teams: tuple[str], year: tuple[int, int]) -> int:
     """Scrape statistics from the desired teams and year dates"""
-    for team in team:
-        for year in range(year[0], year[1]):
+    for team in teams:
+        with open(f"{team}.csv", "w") as file:
+            r = requests.get(f"https://www.basketball-reference.com/teams/{team}/")
+            file.write(r.text)
     return 0

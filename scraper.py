b4b256f8e0c0db24f9e99abff208e7db59c9a3e8
Roadflare <roadflare@tuta.io>
2024-10-20 22:04:52 +0200

    Data analysis

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import json
+import time
 
 ALL_TEAMS = ("ATL", "BOS", "NJN", "CHA", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", 
 "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NOH", "NYK", "OKC", 
@@ -23,11 +23,11 @@ def User_input() -> tuple[tuple, tuple]:
     team = tuple(team)
     return team, year
 
-def Scraper(x: tuple[tuple]) -> dict:
+def Scraper(teams: tuple) -> dict:
     """Scrape statistics from the desired teams and year dates"""
-    teams = x[0]
     data = dict()
     for team in teams:
+        time.sleep(30)
         data[team] = dict()
         request = requests.get(f"https://www.basketball-reference.com/teams/{team}/")
         soup = Table_rows(BeautifulSoup(request.text, 'html.parser'))
@@ -35,7 +35,7 @@ def Scraper(x: tuple[tuple]) -> dict:
             data[team][Season_year(row)] = dict()
             for cell in Table_cells(row):
                 name = cell["data-stat"]
-                if name not in {"DUMMY", "lg_id", "coaches", "top_ws", "rank_team_playoffs"}:
+                if name not in {"DUMMY", "lg_id", "coaches", "top_ws", "rank_team_playoffs", "team_name"}:
                     data[team][Season_year(row)][name] = cell.text
     return data
 
@@ -50,3 +50,37 @@ def Table_cells(row):
 def Season_year(row):
     """Returns the year of the season"""
     return row.find_all("th")[0].text
+
+def Sort_by_stats(teams: dict[str, dict[str, dict[str, int]]], stats: str) -> list[tuple[int, str, str]]:
+    """Returns list of sorted statistic type"""
+    scores: list[tuple[int, str, str]] = []
+    for team, team_val in teams.items():
+        for year, year_val in team_val.items():
+            scores.append((int(year_val[stats]), year, team))
+    return sorted(scores)[::-1]
+
+def Float_Zero(value):
+    """Float conversion with error handling"""
+    try:
+        return float(value)
+    except:
+        return 0.0
+
+def Sort_by_dominance(teams: dict[str, dict[str, dict[str, int]]]) -> list[tuple[int, str, str]]:
+    """Sorts teams by my terms of dominance"""
+    scores: list[tuple[int, str, str]] = []
+    for team, team_val in teams.items():
+        for year, year_val in team_val.items():
+            scores.append(((10 * Float_Zero(year_val["win_loss_pct"]) + (Float_Zero(year_val["off_rtg_rel"]) / 100) + (Float_Zero(year_val["def_rtg_rel"]) / -100)), year, team))
+    return sorted(scores)[::-1]
+
+def Count_occurence(data: list[tuple]) -> dict:
+    """Counts how many times a number is listed in given list"""
+    count = {}
+    for i in data:
+        wins = i[0]
+        if wins in count:
+            count[wins] += 1
+        else:
+            count[wins] = 1
+    return count
